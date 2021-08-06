b24a296293403665b5878945b6411c45f02215c4
Eda Dener <81362322+edadener@users.noreply.github.com>
2021-08-06 11:50:47 +0300

    Update project

---
+++ src/main/java/com/edadener/multithreading/App.java
@@ -0,0 +1,28 @@
+package com.edadener.multithreading;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        final Counter c = new Counter();
+
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    c.increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 1000; i++) {
+                    c.increment();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ src/main/java/com/edadener/multithreading/Counter.java
@@ -0,0 +1,8 @@
+package com.edadener.multithreading;
+
+public class Counter {
+    int count;
+    public void increment(){
+        System.out.println(count++);
+    }
+}
